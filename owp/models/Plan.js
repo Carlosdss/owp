16c74f054ff7ecdfa28b7b4e9b0dab2f1c2815c6
Carlos Dueñas <carlosdss84@gmail.com>
2017-07-19 16:47:13 +0200

    control miércoles 16.30

--- owp/models/Plan.js
+++ owp/models/Plan.js
@@ -5,12 +5,12 @@ const TYPES    = require('./plan-types');
 const PlanSchema = new Schema({
   title : { type: String, required: true },
   description : { type: String, required: true },
-  category : { type: String, enum: TYPES, required: true },
-  creator : { type: Schema.Types.ObjectId, ref: 'User', required: true },
-  date : { type: Number, required: true },
+  category : { type: String, required: true },
+  //creator : { type: Schema.Types.ObjectId, ref: 'User', required: true },
+  date : { type: Date, required: true },
   //location : { type: { type: String }, coordinates: [Number] },
-  //picPath : { type: String, required: true },
-  //picName : { type: String, required: true }
+  picPath : String,
+  picName : String
 });
 
 PlanSchema.index({ location: '2dsphere' });
--- owp/routes/core.js
+++ owp/routes/core.js
@@ -2,6 +2,8 @@ const express    = require("express");
 const router = express.Router();
 const passport = require("passport");
 const ensureLogin = require("connect-ensure-login");
+const multer  = require('multer');
+const upload = multer({ dest: './public/uploads/' });
 
 // Plan model
 const Plan = require("../models/Plan");
@@ -11,11 +13,18 @@ router.get("/newPlan", (req, res, next) => {
 });
 
 
-router.post("/newPlan", (req, res, next) => {
+router.post("/newPlan", upload.single('photo'), function(req, res, next){
   const title = req.body.title;
   const description = req.body.description;
   const category = req.body.category;
   const date = req.body.date;
+
+  console.log(title)
+  console.log(description)
+  console.log(category)
+  console.log(date)
+  console.log(`/uploads/${req.file.filename}`,)
+  console.log(req.file.originalname)
 
   if (title === "" || description === "" || category === "" || date === "") {
     res.render("newPlan", { message: "Please, complete all the fields" });
@@ -26,7 +35,9 @@ router.post("/newPlan", (req, res, next) => {
       title: title,
       description: description,
       category: category,
-      date: date
+      date: date,
+      picPath: `/uploads/${req.file.filename}`,
+      picName: req.file.originalname
     });
 
     newPlan.save((err) => {
